a6ab2f39bde1ae5bf3ae1b29be7593c6d6dfebdf
Fellmaris <cerniauskas.arturas@gmail.com>
2022-03-11 07:49:17 +0200

    Create app, added context-path in app properties

---
+++ src/main/java/com/example/bankaccountbalancerestapi/BankAccountBalanceRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.bankaccountbalancerestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankAccountBalanceRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankAccountBalanceRestApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bankaccountbalancerestapi/BankAccountBalanceRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bankaccountbalancerestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAccountBalanceRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
