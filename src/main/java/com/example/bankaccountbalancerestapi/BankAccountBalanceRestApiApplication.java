9eb9d07dd50fefb15f41e5fceb5dae1884035b47
Fellmaris <cerniauskas.arturas@gmail.com>
2022-03-11 10:55:37 +0200

    Clean up

--- src/main/java/com/example/bankaccountbalancerestapi/BankAccountBalanceRestApiApplication.java
+++ src/main/java/com/example/bankaccountbalancerestapi/BankAccountBalanceRestApiApplication.java
@@ -1,6 +1,5 @@
 package com.example.bankaccountbalancerestapi;
 
-import com.example.bankaccountbalancerestapi.repository.AccountRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
